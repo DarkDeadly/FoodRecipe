5aa925b4c7802f54254fe32082d93551b378d871
DarkDeadly <omrianis2@gmail.com>
2024-12-03 11:23:08 -0800

    adding tailwind css and react-router

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="fullContent">
+            <div className="leftContent"></div>
+            <div className="rightcontent"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
